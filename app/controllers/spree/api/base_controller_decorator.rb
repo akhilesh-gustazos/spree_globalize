d629278f99b1265f8960bb4acc436b6ffdbcff04
Akhilesh Patel <105270681+akhilesh-gustazos@users.noreply.github.com>
2023-03-09 17:23:13 +0530

    Update base_controller_decorator.rb

--- app/controllers/spree/api/base_controller_decorator.rb
+++ app/controllers/spree/api/base_controller_decorator.rb
@@ -1,5 +1,5 @@
 module Spree::Api::BaseControllerDecorator
-  Spree::Api::BaseController.include(SpreeGlobalize::ControllerGlobalizeHelper)
+  Spree::Api::V1::BaseController.include(SpreeGlobalize::ControllerGlobalizeHelper)
 end
 
-::Spree::Api::BaseController.prepend(Spree::Api::BaseControllerDecorator)
+::Spree::Api::V1::BaseController.prepend(Spree::Api::BaseControllerDecorator)
